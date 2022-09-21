0c69ea01edecccb7a09677e14820da7280be8615
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-09-21 17:50:37 -0300

    Requisitos 7, 8 e 9 Finalizados.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-// import Title from './components/Title';
+import Missions from './components/Missions';
 
 import './App.css';
 
@@ -12,7 +12,7 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
-        {/* <Title /> */}
+        <Missions />
       </div>
     );
   }

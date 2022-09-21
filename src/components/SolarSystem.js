159f8ec039914a1a801a9e07b45864def7be017a
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-09-21 16:35:02 -0300

    Requisito 1,2 e 3 Finalizados.

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <header>
         <div data-testid="solar-system" />
+        <Title headline="Planetas" />
       </header>
     );
   }

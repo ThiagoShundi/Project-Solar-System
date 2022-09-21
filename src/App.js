5c8edbe1ce4fef4c67c0505d97c04a6305e2716e
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-09-21 15:20:46 -0300

    Inicio do Projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+// import Title from './components/Title';
+
+import './App.css';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <div>
+        <p>Sistema Solar</p>
+        <Header />
+        <SolarSystem />
+        {/* <Title /> */}
+      </div>
+    );
   }
 }
 
